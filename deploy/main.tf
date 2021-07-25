c741a40a9d5c9c9fd4ad0432f6b4ec9302319280
j-mcneil <mcneil.james.r@gmail.com>
2021-07-25 16:48:50 -0400

    ci: :construction-worker: directly attach dynamodb policy to task role

--- deploy/main.tf
+++ deploy/main.tf
@@ -190,23 +190,13 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-data "aws_iam_policy_document" "ecs_task_role" {
-  version = "2012-10-17"
-  statement {
-    sid     = ""
-    effect  = "Allow"
-    actions = ["dynamodb:*"]
-    
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
 resource "aws_iam_role" "ecs_task_role" {
   name               = "ecs-staging-task-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
 data "template_file" "drawchamp-api" {
